2f149a92f0875b166c3d76a015951f3d1d76c2ec
Sgissa <salmanegarba721@gmail.com>
2025-03-01 20:39:20 -0500

    App.jsx edited to display

--- vite-list/src/App.jsx
+++ vite-list/src/App.jsx
@@ -1,9 +1,49 @@
-import ToDoList from "./ToDoList";
+import { useState } from "react";
+export default function ToDoList() {
+  const [tasks, setTasks] = useState(["eat food", "shower", "walk"]);
+  const [newTask, setNewTask] = useState("");
 
-export default function App() {
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((element, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+  function moveTaskUp(index) {}
+  function moveTaskDown(index) {}
   return (
     <>
-      <ToDoList />
+      <div className="to-do-list">
+        <h1>To Do List</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter a task.."
+            value={newTask}
+            onChange={handleInputChange}
+          />
+          <button className="add-btn" onClick={addTask}>
+            Add
+          </button>
+        </div>
+        <ol>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span className="text">{task}</span>
+              <button className="delete-btn" onClick={() => deleteTask(index)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
     </>
   );
 }
